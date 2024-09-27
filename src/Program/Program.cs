339b96200fd8ad25be02d249717b946c6d533762
zugnoniguz <guzman.zugnoni@correo.ucu.edu.uy>
2024-09-27 12:30:14 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CompAndDel.Pipes;
+using CompAndDel.Filters;
+
+namespace CompAndDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PipeNull pipeNull = new PipeNull();
+            PipeSerial pipeSerial2 = new PipeSerial(new FilterNegative(), pipeNull);
+            PipeSerial pipeSerial1 = new PipeSerial(new FilterGreyscale(), pipeSerial2);
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            IPicture result = pipeSerial1.Send(picture);
+            provider.SavePicture(result, @"luke1.jpg");
+        }
+    }
+}
